8722697d29a9e24f6b1c2015a36cd535ed6b9fe1
chung5072 <icll@naver.com>
2024-04-30 16:39:19 +0900

    feat: 유닛 테스팅 및 e2e 테스팅

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 import { NotFoundException } from '@nestjs/common';
+import exp from 'constants';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -11,7 +12,14 @@ describe('MoviesService', () => {
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+    // 여기서 미리 영화를 생성하면 테스트마다 영화를 생성하지 않아도 된다.
   });
+
+  // 이외에도 다른 함수들이 있음
+  // beforeEach, AfterEach, beforeAll, afterAll ... 
+  afterAll(() => {
+    // 데이터베이스를 싹 비우는 등 그런 기능을 집어 넣을 수 있음
+  })
 
   /**
    * 테스팅 규칙
@@ -61,6 +69,79 @@ describe('MoviesService', () => {
         // ! 기대값인 instance가 해당 클래스에 속하는지
         expect(e).toBeInstanceOf(NotFoundException);
       }
+    })
+  })
+
+  describe("deleteOne()", () => {
+    it("delete a movie", () => {
+      // 삭제 전에 영화를 하나 만듦
+      service.createMovie({
+        // id는 자동으로 생성해주니까
+        title: "test",
+        year: 2024,
+        genres: [
+          "animation", "action"
+        ]
+      });
+      // 만든 후 확인
+      console.log(service.getAll());
+      const allMovies = service.getAll();
+      
+      service.deleteOne(1);
+
+      const afterDelete = service.getAll();
+
+      expect(afterDelete.length).toEqual(allMovies.length - 1);
+    })
+
+    it("should return a 404", () => {
+      try {
+        service.deleteOne(9999999999);
+      } catch (error) {
+        expect(error).toEqual(NotFoundException);
+      }
+    })
+  })
+
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+
+      service.createMovie({
+        // id는 자동으로 생성해주니까
+        title: "test",
+        year: 2024,
+        genres: [
+          "animation", "action"
+        ]
+      })
+
+      console.log(service.getOne(1).title);
+
+      const afterCreate = service.getAll().length;
+
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    })
+  })
+
+  describe("update", () => {
+    it("should update a movie", () => {
+      service.createMovie({
+        // id는 자동으로 생성해주니까
+        title: "test",
+        year: 2024,
+        genres: [
+          "animation", "action"
+        ]
+      });
+
+      service.update(1, {
+        title: "updated test"
+      });
+
+      const updatedMovie = service.getOne(1);
+
+      expect(updatedMovie.title).toEqual("updated test");
     })
   })
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { title } from 'process';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // 이렇게 수정해서 movies를 테스팅하는 동안 애플리케이션 유지
+  // beforeEach(async () => {
+  beforeAll(async() => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    // 테스팅을 진행할 때마다 애플리케이션을 생성함
+    // 실제 서비스에 올라가는 애플리케이션과는 다른 테스팅용 애플리케이션
+    // 이러면 movies의 데이터는 계속해서 초기화됨
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    }))
     await app.init();
   });
 
+  // url 요청에 대한 테스팅
+  // control, service, pip 등 모든 결과에 대한 테스팅
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello Nest!');
   });
+
+  describe("/movies", () => {
+    it("GET", () => {
+      return request(app.getHttpServer())
+      .get("/movies")
+      .expect(200)
+      .expect([]);
+    });
+
+    it("POST", () => {
+      return request(app.getHttpServer())
+      .post("/movies")
+      .send({
+        title: "test",
+        year: 2024,
+        genres: ["test"]
+      })
+      .expect(201); // 201은 생성이 되었다는 뜻
+    })
+
+    it("DELETE", () => {
+      return request(app.getHttpServer()).delete("/movies").expect(404);
+    })
+  });
+
+  describe("/movie/:id", () => {
+    // 어떤 것을 할 것인지 미리 적음
+    // it.todo("GET");
+    it("GET 200", () => {
+      // url에서 1은 string 타입
+      // 메인 애플리케이션에서 파이프를 통해 transform을 적용시켜 타입을 변환시켰었음
+      // 테스팅에서도 똑같이 적용을 시켜야 함
+      return request(app.getHttpServer())
+      .get("/movies/1")
+      .expect(200);
+    })
+    it("GET 404", () => {
+      return request(app.getHttpServer())
+      .get("/movies/99999999")
+      .expect(404);
+    })
+
+    // it.todo("POST");
+    it("POST 400", () => {
+      return request(app.getHttpServer())
+      .post("/movies").send({hacked: "fuckyeah!"})
+      .expect(400);
+    })
+
+    // it.todo("PATCH");
+    it("PATCH", () => {
+      return request(app.getHttpServer())
+      .patch("/movies/1").send({title: "updated test"})
+      .expect(200); // 삭제도 200
+    })
+
+    // it.todo("DELETE");
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+      .delete("/movies/1")
+      .expect(200);
+    })
+  })
 });
